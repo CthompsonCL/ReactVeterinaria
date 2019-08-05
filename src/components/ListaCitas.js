18061e6adeb7acdd0e34b13a157e1dd5d9526664
Christopher Thompson Huerta <c.thompsonhuerta@gmail.com>
2019-08-05 01:24:56 -0400

    Se agrega perfil de deployment para sitio en producción.

--- src/components/ListaCitas.js
+++ src/components/ListaCitas.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import Cita from './Cita';
+import PropTypes from 'prop-types';
 
-const ListaCitas = ({citas,eliminarCita}) => (
+const ListaCitas = ({citas,eliminarCita}) => {
+  
+  const mensaje = Object.keys(citas).length === 0 ? 'No hay citas' : 'Administrar las citas aquí';
+
+    return (
     
         <div className="card mt-2 py-5">
             <div className="card-body ">
-                <h2 className="card-title text-center">
-                    Administra las citas aquí
-                </h2>
+                <h3 className="card-title text-center">
+                   {mensaje}
+                </h3>
             <div className="lista-citas">
                     {citas.map(cita => (
                         <Cita
@@ -20,5 +25,15 @@ const ListaCitas = ({citas,eliminarCita}) => (
             </div>
         </div>
 )
- 
+}
+
+
+
+ListaCitas.propTypes = {
+    citas: PropTypes.array.isRequired,
+    eliminarCita : PropTypes.func.isRequired
+}
+
+
+
 export default ListaCitas;
--- src/components/NuevaCita.js
+++ src/components/NuevaCita.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import uuid from 'uuid'; 
-
+import PropTypes from 'prop-types';
 
 const stateInicial = {
    
@@ -162,6 +162,10 @@ class NuevaCita extends Component {
             </div>
         );
     }
+}
+
+NuevaCita.propTypes = {
+    crearNuevaCita: PropTypes.func.isRequired
 }
 
 export default NuevaCita;
